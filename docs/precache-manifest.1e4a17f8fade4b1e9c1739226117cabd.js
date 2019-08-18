532060a45627f3669d7f822cb40aea836a4bbef7
pixelmary <maria.hojas@gmail.com>
2019-08-18 16:31:10 +0200

    Add docs folder to master, exercise 6

---
+++ docs/precache-manifest.1e4a17f8fade4b1e9c1739226117cabd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0205a31b65e8168238b9eaacb8cca51e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6762add6bebf18972e08",
+    "url": "/static/css/main.2d54dcf7.chunk.css"
+  },
+  {
+    "revision": "bdaa3003a1f26b085b13",
+    "url": "/static/js/2.483d6199.chunk.js"
+  },
+  {
+    "revision": "6762add6bebf18972e08",
+    "url": "/static/js/main.e323568b.chunk.js"
+  },
+  {
+    "revision": "0a7b9e75e89f68de5d8a",
+    "url": "/static/js/runtime~main.a71a2347.js"
+  },
+  {
+    "revision": "1a0d86a76f4442c8b8a6ca8b68c279bc",
+    "url": "/static/media/avatar.1a0d86a7.jpg"
+  }
+]);
